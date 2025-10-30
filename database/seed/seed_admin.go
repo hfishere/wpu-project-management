2ded93116ae9dadc17bf048821b9c39634e6cd56
hfishere <hafidzfairiz@gmail.com>
2025-10-30 13:48:05 +0700

    Initial Commit

---
+++ database/seed/seed_admin.go
@@ -0,0 +1,26 @@
+package seed
+
+import (
+	"log"
+
+	"github.com/hfishere/wpu-project-management/config"
+	"github.com/hfishere/wpu-project-management/models"
+	"github.com/hfishere/wpu-project-management/utils"
+)
+
+func SeedAdmin() {
+	password, _ := utils.HashPassword("admin123")
+
+	admin := models.User{
+		Name:     "Super Admin",
+		Email:    "admin@example.com",
+		Password: password,
+		Role:     "admin",
+	}
+
+	if err := config.DB.FirstOrCreate(&admin, models.User{Email: admin.Email}).Error; err != nil {
+		log.Println("Failed to seed admin", err)
+	} else {
+		log.Println("Admin user seeded.")
+	}
+}
---
+++ models/list.go
@@ -0,0 +1,17 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type List struct {
+	InternalID    int64     `json:"internal_id" db:"internal_id" gorm:"column:internal_id;primaryKey;autoIncrement"`
+	PublicID      uuid.UUID `json:"public_id" db:"public_id"`
+	Title         string    `json:"title" db:"title"`
+	Position      int64     `json:"position" db:"position"`
+	CreatedAt     time.Time `json:"created_at" db:"created_at"`
+	BoardPublicID uuid.UUID `json:"board_public_id" db:"board_public_id" gorm:"column:board_public_id"`
+	BoardID       int64     `json:"-" db:"board_internal_id"`
+}
